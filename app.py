4cce63a53405e2f0a1af356229b04653f5f2a0e0
paigevenuto <paigevenuto@gmail.com>
2020-10-03 21:09:13 -0400

    another typo :D

--- app.py
+++ app.py
@@ -220,7 +220,7 @@ def test():
     username = get_session_user()
     user = get_user(username)
     credentials = ytmapi.get_credentials(user.id)
-    likes = ytmapi.get_liked_videos(token.token)
+    likes = ytmapi.get_liked_videos(credentials)
     ytmapi.save_liked_videos(likes, user)
     return 'hopefully this just saved likes to the db'
 
